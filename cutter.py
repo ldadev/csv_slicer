ec1e7530e06dc7d91162c1a46cd0dbedbfec12b8
David martinez <ldavidme7@gmail.com>
2022-01-21 00:15:23 -0500

    Se termino la clase. Solo falta el residuo

--- cutter.py
+++ cutter.py
@@ -4,8 +4,36 @@ class slicers:
 
 	def __init__ (self,cut_lenght):
 
-		self.source_file =  pd.read_csv('csv_files/test_file.csv')
-		self.cut_lenght = cut_lenght
+		self.source_file =  pd.read_csv('csv_files/test_file.csv')# Fuente del archivo csv
+		self.num_records = self.source_file.shape[0] #Total de registros
+		self.cut_lenght = cut_lenght # Ancho del corte
+
+
+	def __str__(self):
+
+		print(self.source_file,self.num_records,self.cut_lenght)
+
+
+	
+	def segment_file(self,initial_cut,end_cut):
+
+		self.initial_cut = initial_cut
+		self.end_cut = end_cut
+
+		end_segment = self.source_file[int(initial_cut):int(end_cut)]
+
+		msg = 'output_files/' + f'Segment {initial_cut} to {end_cut}'
+		end_segment.to_csv(msg + '.csv',index=False) # Guardamos los reccortes
+
+
+
+
+
+
+
+
+
+
 
 
 
--- main.py
+++ main.py
@@ -3,6 +3,20 @@ from cutter import slicers
 if __name__== "__main__":
 
 	slicer = slicers(100)
-	slicer.__str__()
+
+	for cutting_interval in range(1,slicer.num_records +50,slicer.cut_lenght):
+		
+		if (cutting_interval > slicer.cut_lenght) and cutting_interval <= slicer.num_records +50:
+
+			initial_cut = cutting_interval - (slicer.cut_lenght)
+			end_cut  = cutting_interval
+			if end_cut > slicer.num_records:
+				slicer.segment_file(initial_cut + 1,slicer.num_records)
+			else:
+				slicer.segment_file(initial_cut,end_cut)
+
+
+
+
 
 
