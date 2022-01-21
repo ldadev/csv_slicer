458557daef4253fb706e03c6002a5b9453bccf90
David martinez <ldavidme7@gmail.com>
2022-01-20 21:45:01 -0500

    Se creo la carpeta para los archivos csv y el constructor de la clase padre

--- cutter.py
+++ cutter.py
@@ -1,12 +1,23 @@
 import pandas as pd
 
-class slicer:
+class slicers:
 
-	def _init__ (self,source_file,cut_lenght):
+	def __init__ (self,cut_lenght):
 
 		self.source_file =  pd.read_csv('csv_files/test_file.csv')
 		self.cut_lenght = cut_lenght
-		
+
+
+
+	def __str__(self):
+
+		print(f'Logitud del corte {self.cut_lenght} y archivo fuente \n {self.source_file.head}')
+
+
+
+
+
+
 
 
 
---
+++ main.py
@@ -0,0 +1,8 @@
+from cutter import slicers
+
+if __name__== "__main__":
+
+	slicer = slicers(100)
+	slicer.__str__()
+
+
